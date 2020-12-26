ebecdf8511d5353b28b32e974a764b4d8eeecc80
nloves <894765963@qq.com>
2020-12-26 15:58:22 +0800

    Create index.js

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+const library = require('./library')
+const user = require('./user')
+const mygeneral = require('./mygeneral')
+const tree = require('./tree')
+const views = require('./views')
+const shediao = require('./shediao')
+
+exports.library = library
+exports.user = user
+exports.mygeneral = mygeneral
+exports.tree = tree
+exports.views = views
+exports.shediao = shediao
