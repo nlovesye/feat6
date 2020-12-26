c2281dcebce3e9be4f175dedb542f723b449b82e
nloves <894765963@qq.com>
2020-12-26 16:01:59 +0800

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const router = require('koa-router')()
+const { db } = require('../utils')
+
+router.get('/', async (ctx, next) => {
+    // db.addJson('test')
+    // db.insert('test', general)
+    // db.removeAll('test')
+    const str = db.findJson('nloves')
+    // console.log('str', str)
+    await ctx.render('index', {
+        // title: 'Hello Koa 2!',
+        // content: str ? JSON.stringify(str) : ''
+    })
+})
+
+module.exports = router
