0f092ab6b8662a7afbbd41ece1f04ef3cd08ce37
nloves <894765963@qq.com>
2021-03-15 15:54:06 +0800

    Create testData.js

---
+++ web/src/views/main/game/testData.js
@@ -0,0 +1,18 @@
+/* eslint-disable */ 
+export default {
+    "edit": {
+        name: '南阳渡',
+        "k": true,
+        "m": true,
+        "y": true,
+        "b": true,
+        "n": true,
+        "z": false,
+        "l": true,
+        "t": true,
+        "j": false
+    },
+    "batch": {
+        specDesc: 'aaaaaaaaaaa'
+    }
+}
